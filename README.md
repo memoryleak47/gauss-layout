dddd89262e81cf4aff0fc2464b69732f2c553400
memoryleak47 <memoryleak47@protonmail.ch>
2025-12-26 22:14:10 +0100

    make example more interesting

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Gauss Layouting
 ===============
 
 A proof-of-concept layouting engine based on Gaussian Elimination.
+Have a look at `example.py` to see how to use this layouting engine.
--- example.py
+++ example.py
@@ -17,11 +17,17 @@ gs.add(b1.center_y == gs.window.center_y)
 gs.add(b2.center_y == gs.window.center_y)
 gs.add(b3.center_y == gs.window.center_y)
 
-# They have spaces of 10 in between and fill the entire x-space
-gs.add(b1.left == gs.window.left + 10)
-gs.add(b1.right + 10 == b2.left)
-gs.add(b2.right + 10 == b3.left)
-gs.add(b3.right + 10 == gs.window.right)
+# In x-direction, the boxes are positioned in sequence.
+in_gap = gs.Var() # Gap between consecutive boxes
+out_gap = gs.Var() # Gap between window border and box
+
+gs.add(b1.left == gs.window.left + out_gap)
+gs.add(b1.right + in_gap == b2.left)
+gs.add(b2.right + in_gap == b3.left)
+gs.add(b3.right + out_gap == gs.window.right)
+
+gs.add(in_gap == out_gap / 2) # the distance to the border should be double of the distance between boxes
+gs.add(h == in_gap*4) # the gap between elements shall be a fourth of the height of the boxes
 
 # They have the same width
 gs.add(b1.width == b2.width)
